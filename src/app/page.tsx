61b13ed5379d93ab7ab6904208f17bfdb8d10afb
Oleh Abanin <oleh@outvio.com>
2023-09-18 21:29:46 +0300

    remove rewrite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,8 @@
 import Image from 'next/image'
-import Script from "next/script";
 
 export default function Home() {
   return (
       <>
-
-          <Script id="githubpages">
-            {`(function(l) {
-        if (l.search[1] === '/' ) {
-          var decoded = l.search.slice(1).split('&').map(function(s) { 
-            return s.replace(/~and~/g, '&')
-          }).join('?');
-          window.history.replaceState(null, null,
-              l.pathname.slice(0, -1) + decoded + l.hash
-          );
-        }
-      }(window.location))`}
-          </Script>
 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
